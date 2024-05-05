df7b18d3c7947aaeb71fb344f377d21bfe8c776e
vaskozlov <vaskozlov3940@gmail.com>
2024-05-05 17:54:21 +0300

    feat: getTextFieldWithKeyListener has been moved to adapters && support call on specific key pressed or when it goes out of focus

--- src/main/kotlin/ui/lib/DevicesAdapters.kt
+++ src/main/kotlin/ui/lib/DevicesAdapters.kt
@@ -3,6 +3,7 @@ package ui.lib
 import java.awt.event.*
 import javax.swing.AbstractAction
 import javax.swing.JButton
+import javax.swing.JTextField
 
 fun keyboardKeyReleasedAdapter(action: (e: KeyEvent) -> Unit): KeyAdapter =
     object : KeyAdapter() {
@@ -28,3 +29,32 @@ fun buttonClickAdapter(action: (e: ActionEvent) -> Unit): JButton =
             action(e)
         }
     })
+
+fun getTextFieldWithKeyListener(size: Int, keyCode: Int?, action: (JTextField) -> Unit) =
+    object : JTextField(size) {
+        private val targetKeyCode = keyCode
+
+        private fun forceGetThis(): JTextField {
+            return this
+        }
+
+        init {
+            addKeyListener(
+                keyboardKeyReleasedAdapter {
+                    if (targetKeyCode == null || it.keyCode == targetKeyCode) {
+                        action(this)
+                    }
+                }
+            )
+
+            addFocusListener(object : FocusListener {
+                override fun focusGained(e: FocusEvent?) {
+                    // empty
+                }
+
+                override fun focusLost(e: FocusEvent?) {
+                    action(forceGetThis())
+                }
+            })
+        }
+    }
